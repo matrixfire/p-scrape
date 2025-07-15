de5adb16a27836b088f783d2b3c9de86e89ce970
matrixfire <48238435+matrixfire@users.noreply.github.com>
2025-07-15 11:21:42 +0800

    OK

--- config.py
+++ config.py
@@ -4,19 +4,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 MONGO_URI = os.getenv('MONGO_URI', 'mongodb://localhost:27017/')
-DB_NAME = os.getenv('DB_NAME', 'blog_database')
-COLLECTION_NAME = os.getenv('COLLECTION_NAME', 'blog4')
-
 SCRAPED_DB_NAME = os.getenv('SCRAPED_DB_NAME', 'scraped_db')
-# SCRAPED_COLLECTION_NAME = os.getenv('SCRAPED_COLLECTION_NAME', 'products')
-SCRAPED_COLLECTION_NAME = os.getenv('SCRAPED_COLLECTION_NAME', 'products_US_REST')
-
-def get_mongo_config():
-    return {
-        'MONGO_URI': MONGO_URI,
-        'DB_NAME': DB_NAME,
-        'COLLECTION_NAME': COLLECTION_NAME
-    }
+SCRAPED_COLLECTION_NAME = os.getenv('SCRAPED_COLLECTION_NAME', 'products_cn_1')
 
 def get_scraped_db_config():
     return {
