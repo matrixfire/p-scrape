fe0aecb40f06d24021982cf6361569f8bc650c10
matrixfire <48238435+matrixfire@users.noreply.github.com>
2025-07-15 19:28:33 +0800

    OK

--- scrape_product_list_async.py
+++ scrape_product_list_async.py
@@ -541,7 +541,7 @@ async def scrape_multiple_urls(urls, collection, tracker, max_concurrent_details
 
 if __name__ == "__main__":
     collection = init_mongo_scraped()
-    with open("diff_cn.json", "r", encoding='utf-8') as f:
+    with open("clothing.json", "r", encoding='utf-8') as f:
         tasks = json.load(f)
 
     tracker = TaskTracker(tasks, id_key='url')
