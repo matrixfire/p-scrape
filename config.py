62106fe8ff047bbaf511918c0bb4b6700b6c2cf0
matrixfire <48238435+matrixfire@users.noreply.github.com>
2025-07-22 15:49:15 +0800

    OK

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 MONGO_URI = os.getenv('MONGO_URI', 'mongodb://localhost:27017/')
 SCRAPED_DB_NAME = os.getenv('SCRAPED_DB_NAME', 'scraped_db_new')
-SCRAPED_COLLECTION_NAME = os.getenv('SCRAPED_COLLECTION_NAME', 'products_tx')
+SCRAPED_COLLECTION_NAME = os.getenv('SCRAPED_COLLECTION_NAME', 'products_hats_cn')
 
 def get_scraped_mongodb_config():
     return {
@@ -23,7 +23,7 @@ db_config = {
     "port": 28745,
 }
 
-tasks_json = 'diff_cn_clothing_shoes.json'
+tasks_json = 'diff_cn_hats.json'
 
 cj_config = {
 'cj_account': 'tychan@163.com',
